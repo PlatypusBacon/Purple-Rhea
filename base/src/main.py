a4bf66b90c52e2e918cd8c9bfb0ca82606f9c254
Tyler <s4743458@student.uq.edu.au>
2026-05-27 10:02:08 +1000

    back

--- base/src/main.py
+++ base/src/main.py
@@ -10,9 +10,9 @@ import config
 
 def main(on_frame_captured=None, on_pipeline_progress=None):
     print("Base Node running")
-    #session = start_session(on_frame_captured=on_frame_captured)
+    session = start_session(on_frame_captured=on_frame_captured)
     #session = start_disk_ble_session(on_frame_captured=on_frame_captured)
-    session = start_disk_session()
+    #session = start_disk_session()
 
 
     if not session.is_complete():
